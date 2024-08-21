17678752137442e3749f1a0cb581c962f99c0541
gagliardivi <vicky.gagliardirocha@gmail.com>
2024-08-20 21:54:23 -0300

    Ejercicio

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+    var carga = document.getElementById("carga");
+
+        carga.style.display="block"
+           
+    setTimeout (() => {
+            carga.style.display=" "
+       }, 1000) ;
+    
+    });
+}
+
